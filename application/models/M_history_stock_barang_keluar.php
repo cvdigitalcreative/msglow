b70b7758acf131dae0db408cc7157f49838535de
malian <malianzikri92@gmail.com>
2021-06-14 19:18:46 +0700

    bug fixing

--- application/models/M_history_stock_barang_keluar.php
+++ application/models/M_history_stock_barang_keluar.php
@@ -13,7 +13,7 @@
 		}
 		function getHistoryStock_all_by_tanggal($id_toko){
 			$tanggal = date("Y-m-d");
-			$hasil=$this->db->query("SELECT barang.barang_nama,history_stok_barang_keluar.barang_id,barang_stock_keluar as stok_awal_hari,sum(stok_keluar) as total_barang_keluar,(barang_stock_keluar-sum(stok_keluar)) as stok_akhir_hari,DATE(date) as tanggal FROM `history_stok_barang_keluar`,barang where history_stok_barang_keluar.barang_id=barang.barang_id and DATE(date)='$tanggal' and history_stok_barang_keluar.id_toko='$id_toko'  GROUP by DATE(date),history_stok_barang_keluar.barang_id  
+			$hasil=$this->db->query("SELECT barang.barang_nama,history_stok_barang_keluar.barang_id,barang_stock_keluar as stok_awal_hari,sum(stok_keluar) as total_barang_keluar,(barang_stock_keluar-sum(stok_keluar)) as stok_akhir_hari,DATE(date) as tanggal,toko.nama as nama_toko  FROM `history_stok_barang_keluar`,barang,toko where history_stok_barang_keluar.barang_id=barang.barang_id and DATE(date)='$tanggal' and history_stok_barang_keluar.id_toko=toko.id_toko  and history_stok_barang_keluar.id_toko='$id_toko'  GROUP by DATE(date),history_stok_barang_keluar.barang_id  
 				ORDER BY `history_stok_barang_keluar`.`date`  Desc ");
         	return $hasil;
 		}
